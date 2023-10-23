a5a57835434dea3adf5c5babe55c29a15ead6d96
jago-1009 <garwoodj22@gmail.com>
2023-10-23 10:54:25 -0400

    Began Jungle-Cook

---
+++ app/app.js
@@ -0,0 +1,40 @@
+function initListeners() {}
+ 
+$(document).ready(function () {
+initListeners();
+});
+
+function changeRoute() {
+let hashTag = window.location.hash;
+ let pageID = hashTag.replace('#', '');
+//   console.log(hashTag + ' ' + pageID);
+
+if (pageID != '') {
+$.get(`pages/pageID/pageID.html`, function (data) {
+ console.log('data ' + data);
+ $('#app').html(data);
+});
+} else {
+$.get(`pages/home/home.html`, function (data) {
+console.log('data ' + data);
+ $('#app').html(data);
+});
+}
+}
+
+function initURLListener() {
+$(window).on('hashchange', changeRoute);
+changeRoute();
+}
+
+$(document).ready(function () {
+initURLListener();
+});
+
+
+
+
+
+function toggleMenu(menu) {
+    menu.classList.toggle("open")
+}
