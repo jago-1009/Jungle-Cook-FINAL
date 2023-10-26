515edd0e1b2b2eca367c59dd4ca168302b85c5d6
jago-1009 <garwoodj22@gmail.com>
2023-10-26 17:47:29 -0400

    Started Input for Login

--- app/app.js
+++ app/app.js
@@ -1,7 +1,5 @@
 import { changePage } from "../services/model.js";
 
-
-
 function route() {
     let hashtag = window.location.hash;
     let pageID = hashtag.replace("#","");
@@ -12,31 +10,8 @@ function route() {
 }
 
 function initListeners() {
-    
-    $(".login").on("click", (e) => {
-        if (userID != "" || pass != "") {
-            $("#signed-in").toggle();
-        }
-        else {
-        $("#modal").toggle()
-        }
-    })
-     $(".close").on("click",(e) => {
-        $("#modal").toggle();
-    })
-    $("#submit").on("click",(e) => {
-LogIn();
-    })
-    $("#logout").on("click",(e) => {
-        $("#signed-in").toggle();
-        LogOut()
-    })
-      $(".sign-close").on("click",(e) => {
-        $("#signed-in").toggle();
-    })
+    console.log()
 }
-
-
 function initSite(){
     $(window).on("hashchange",route)
     route();
