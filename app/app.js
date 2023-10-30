228ea49949651b8ea7e32e34748bc22b6b12a21e
jago-1009 <garwoodj22@gmail.com>
2023-10-30 12:29:17 -0400

    Reverted to previous iteration.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,41 @@
-import { changePage } from "../services/model.js";
 
+
+function changePage(pageID) {
+
+        if (pageID =="") {
+            $.get(`pages/home.html`, (data) => {
+                $("#app").html(data)
+            }).fail(() =>  {
+                alert("FAIL")
+            })
+        $("#app").html("home");
+        console.log("HOME HOME HOME")
+         $("#bread-crumb").html(``)
+        
+    } else {
+        
+        if(pageID.length == 1 ) {
+             $("#bread-crumb").html(``)
+             $.get(`pages/${pageID}.html`, (data) => {
+                $("#app").html(data)
+            }).fail(() =>  {
+                alert("ERROR 404: PAGE NOT FOUND")
+            
+            })
+        }
+        else {
+             $("#bread-crumb").html(`<a href="#${pageID[0]}">${pageID[0]}/</a>${pageID[1]}`)
+             $.get(`pages/${pageID[1]}.html`, (data) => {
+                $("#app").html(data)
+            }).fail(() =>  {
+                alert("ERROR 404: PAGE NOT FOUND")
+            
+            })
+        }
+  
+            
+    }
+}
 function route() {
     let hashtag = window.location.hash;
     let pageID = hashtag.replace("#","");
@@ -21,7 +57,6 @@ $(document).ready(function () {
 initListeners();
 initSite();
 });
-
 
 
 
